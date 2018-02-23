98514e2ccafd4824933e16b3cacb6907d5c479cb
Rima <rmhrok@gmail.com>
2018-02-23 06:55:21 +0000

    working on test

--- lab-rima/src/__test__/note-form.test.js
+++ lab-rima/src/__test__/note-form.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { mount } from 'enzyme';
 require('jest');
 
 Enzyme.configure({adapter: new Adapter()});
@@ -9,11 +10,17 @@ import NoteForm from '../components/note-create-form/index';
 
 describe('NoteForm component', () => {
 
-  test('Testing initial state', () => {
+  test('initial state', () => {
     let mountedNoteForm = Enzyme.mount(<NoteForm />);
 
     expect(mountedNoteForm.state('title')).toEqual('');
     expect(mountedNoteForm.state('content')).toEqual('');
   });
 
+/*  test('should set state', () => {
+    const wrapper = mount(
+      <NoteForm />
+    ));
+  });
+*/
 });
