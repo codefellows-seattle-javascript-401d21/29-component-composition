b9015054f62e308a09545e030e900b516522df91
Koko Kassa <kowserk7@gmail.com>
2018-03-18 18:00:54 -0700

    fixed up code

--- lab-koko/README.md
+++ lab-koko/README.md
@@ -5,6 +5,12 @@
 ## Overview
 Creating NoteApp using webpack and react
 
+## How to Use and Install
+- Clone the repository to your local directory
+- Install all the necessary modules using the `npm install` command
+- Run `npm run watch`, open a browser to http://localhost:8080
+
+
 #### Configuration
 * `package.json`
 * `.eslintrc`
--- lab-koko/src/__test__/dashboard.test.js
+++ lab-koko/src/__test__/dashboard.test.js
@@ -5,21 +5,12 @@ import Adapter from 'enzyme-adapter-react-16';
 require('jest');
 
 Enzyme.configure({adapter: new Adapter()});
-import Dashboard from '../component/dashboard/index';
+import Dashboard from '../components/dashboard/dashboard';
 import NoteCreateForm from '../components/dashboard/dashboard';
 
 describe('Dashboard', () => {
   test('Testing initial state', () => {
     let mountedDashboard = Enzyme.mount(<Dashboard />);
     expect(mountedDashboard.state('notes')).toEqual([]);
-  });
-});
-
-describe('NoteCreateForm', () => {
-  test('Testing initial state', () => {
-    let mountedNoteCreateForm = Enzyme.mount(<NoteCreateForm />);
-    expect(mountedNoteCreateForm.state('completed')).toEqual(false);
-    expect(mountedNoteCreateForm.state('content')).toEqual('');
-    expect(mountedNoteCreateForm.state('title')).toEqual('');
   });
 });
